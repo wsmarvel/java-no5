e84faff752fdfefbd130f78052b17d7620a3c606
bye <bye@bye-PC>
2018-04-17 16:44:26 +0800

    java-no5

---
+++ src/num5/InstanceCounter.java
@@ -0,0 +1,33 @@
+package num5;
+
+public class InstanceCounter {
+	private static int numInstances = 0;
+	protected static int getCount() {
+		return numInstances;
+		
+	}
+	
+	private static void addInstance() {
+		numInstances++;
+		
+	}
+	
+	InstanceCounter(){
+		InstanceCounter.addInstance();
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Starting with "+
+		InstanceCounter.getCount()+" instances");
+		for(int i=0;i<1;++i) {
+			new InstanceCounter();
+			}
+		System.out.println("Created "+
+		InstanceCounter.getCount()+" instances");
+	}
+		
+}
+
+
+	
